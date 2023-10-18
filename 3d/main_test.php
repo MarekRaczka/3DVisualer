a46ea492f61db839530821ceca1b1c7f98339427
MarekRaczka <grzegorzslomka08@gmail.com>
2023-10-18 22:54:47 +0200

    tworzenie funkcji matematycznej

--- 3d/main_test.php
+++ 3d/main_test.php
@@ -1,94 +1,120 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Kartezjański układ współrzędnych z funkcją</title>
-    <style>
-        #canvas {
-            border: 1px solid black;
-        }
-    </style>
+<!doctype html>
+<html><head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+<title>Rysowanie wykresów funkcji 2d</title>
 </head>
-<body>
-    <h2>Podaj funkcję: </h2>
-    <input type="text" id="functionInput" placeholder="np. x*x+2*x-3">
-    <button onclick="drawFunction()">Narysuj</button>
-    <br>
-    <h2>Przedział osi X:</h2>
-    <label for="minX">Min X:</label>
-    <input type="number" id="minX" value="-10">
-    <label for="maxX">Max X:</label>
-    <input type="number" id="maxX" value="10">
-    <br>
-    <h2>Przedział osi Y:</h2>
-    <label for="minY">Min Y:</label>
-    <input type="number" id="minY" value="-10">
-    <label for="maxY">Max Y:</label>
-    <input type="number" id="maxY" value="10">
-    <br>
-    <canvas id="canvas" width="400" height="400"></canvas>
+	<body>
+		<canvas style="border:1px solid black;" id="obrazek" width="800" height="600"></canvas>
+		<input type="text" name="function" id="function" value="" />
+		<input type="submit" name="button" id="button" value="Rysuj" />
+		<button id="clear">czyść</button><br />
+		Kolor linii: <select id="color">
+		<option value="green">Zielony</option>
+		<option value="red">Czerwony</option>
+		<option value="blue">Niebieski</option>
+		</select><br/>
+		<script type="text/javascript" src="jquery-1.4.2.js"></script>
+		<script type="text/javascript" src="main.js"></script>
+	</body>
+</html> 
 
-    <script>
-        var canvas = document.getElementById('canvas');
-        var ctx = canvas.getContext('2d');
-        var centerX = canvas.width / 2;
-        var centerY = canvas.height / 2;
-        var scaleX = 20;
-        var scaleY = 20;
+<script>
 
-        function clearCanvas() {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-        }
+var canvas = document.getElementById('obrazek');
+var ctx = canvas.getContext('2d');
+var w = canvas.width;
+var h = canvas.height;
+var range = 100;
+var bl = 1;
+var rangex = w/range;
+var rangey = h/range;
+var color = "green";
 
-        function drawAxes() {
-            ctx.beginPath();
-            ctx.moveTo(0, centerY);
-            ctx.lineTo(canvas.width, centerY);
-            ctx.moveTo(centerX, 0);
-            ctx.lineTo(centerX, canvas.height);
-            ctx.strokeStyle = "black";
-            ctx.stroke();
+function grid()
+{
+	ctx.strokeStyle = "black";
+	for(i=1;i<=Math.floor(rangex*2);i++)
+	{
+		ctx.lineWidth = 1/6;
+		ctx.beginPath();
+		ctx.moveTo((range/2)*i,0);
+		ctx.lineTo((range/2)*i,h);
+		ctx.closePath();
+		ctx.stroke();
+	}
+	for(i=1;i<=Math.floor(rangey*2);i++)
+	{
+		ctx.lineWidth = 1/6;
+		ctx.beginPath();
+		ctx.moveTo(0,(range/2)*i);
+		ctx.lineTo(w,(range/2)*i);
+		ctx.closePath();
+		ctx.stroke();
+	}
+	ctx.lineWidth = 1;
+	ctx.beginPath();
+	ctx.moveTo(0,h/2);
+	ctx.lineTo(w,h/2);
+	ctx.closePath();
+	ctx.stroke();
+ 
+	ctx.beginPath();
+	ctx.moveTo(w/2,0);
+	ctx.lineTo(w/2,h);
+	ctx.closePath();
+	ctx.stroke();
+	for(i=-(rangex/2);i<=(rangex/2);i++)
+	{
+		ctx.fillText(i,(i+(rangex/2))*range,h/2);
+	}
+	ctx.save();
+}
+grid();
 
-            // Oznaczenia na osi X
-            var minX = parseFloat(document.getElementById('minX').value);
-            var maxX = parseFloat(document.getElementById('maxX').value);
-            var minY = parseFloat(document.getElementById('minY').value);
-            var maxY = parseFloat(document.getElementById('maxY').value);
-            var stepX = 1;
-            var stepY = 1;
+ctx.closePath();
+ctx.stroke();
 
-            for (var i = Math.floor(minX); i <= Math.ceil(maxX); i += stepX) {
-                var xPos = centerX + i * scaleX;
-                ctx.fillText(i, xPos, centerY + 15);
-            }
+function start_settings()
+{
+	ctx.translate(w/2, h/2);
+	ctx.scale(range,range);
+}
+start_settings();
 
-            // Oznaczenia na osi Y
-            for (var j = Math.floor(minY); j <= Math.ceil(maxY); j += stepY) {
-                var yPos = centerY - j * scaleY;
-                ctx.fillText(j, centerX + 5, yPos);
-            }
-        }
+function plot()
+{
+	ctx.strokeStyle = color;
+	ctx.lineWidth = 1/50;
+	var f = document.getElementById("function").value;
+	var fun1 = new Function("x", "return "+f);
+	ctx.beginPath();
+	for(x=-rangex;x<=rangex;x+=0.01)
+	{
+		y = fun1(x);
+		if(y!='')
+		{
+			if (isNaN(y) || (y == Number.NEGATIVE_INFINITY) ||
+			(y == Number.POSITIVE_INFINITY) || (Math.abs(y) > 2e5)) {
+			bl = 2;
+			y = 0.0;
+			}
+			if (bl > 0)
+			{
+				if (bl == 1)
+				{
+					ctx.moveTo(x, -y);
+				}
+				--bl;
+			}
+			else {
+				ctx.lineTo(x, -y);
+			}
+		}
+ 
+	}
+	ctx.stroke();
+}
 
-        function drawFunction() {
-            clearCanvas();
-            drawAxes();
+document.getElementById("button").onclick = plot;
 
-            var functionInput = document.getElementById('functionInput').value;
-            var minX = parseFloat(document.getElementById('minX').value);
-            var maxX = parseFloat(document.getElementById('maxX').value);
-
-            ctx.beginPath();
-            var x = minX;
-            var step = 0.01;
-
-            while (x <= maxX) {
-                var y = -eval(functionInput) * scaleY;
-                ctx.lineTo(centerX + x * scaleX, centerY + y);
-                x += step;
-            }
-
-            ctx.strokeStyle = "blue";
-            ctx.stroke();
-        }
-    </script>
-</body>
-</html>
+</script>
